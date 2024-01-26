5335e1b3703c9e50469ab412b63016fa3b7b3a02
alex32680 <144953595+alex32680@users.noreply.github.com>
2024-01-26 04:04:44 +0000

    Domain Generator

---
+++ index.js
@@ -0,0 +1,19 @@
+let multiplictionTable =[];
+
+for (let i=1; i<11; i++){
+    for(let j=1;j<11; j++) 
+    multiplictionTable.push('${i} * ${i*j}')
+}
+
+    let randomNumber = Math.random() *10;
+    console.(multiplictionTable)
+
+   let pronoun = ['the','our','very','cool', 'super'];
+   let adj = ['great','big','tech', 'stack', 'geeks', 'computing'];
+   let noun = ['jogger', 'racoon'];
+
+   for (let i=1; i<11; i++){
+    for(let j=1;j<11; j++) 
+    multiplictionTable.push('${i} * ${j} = ${*j}')
+   }
+   
